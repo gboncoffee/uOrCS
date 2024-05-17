67b7c098a2b5318d18d8558b9fedd7ad38d5f0a8
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2024-05-17 10:20:38 -0300

    Implemented conditionals to the BTB

--- btb.cpp
+++ btb.cpp
@@ -12,16 +12,25 @@ btb_t::btb_t() {
             this->buffer[i][j].valid = false;
         }
     }
+
     this->n_calls = 0;
     this->n_syscalls = 0;
     this->n_uncond = 0;
-    this->n_cond = 0;
     this->n_missed_uncond = 0;
     this->n_missed_calls = 0;
     this->n_missed_syscalls = 0;
+
+    this->n_cond_taken = 0;
+    this->n_cond_not_taken = 0;
+    this->n_cond_taken_missed = 0;
+    this->n_cond_not_taken_missed = 0;
+    this->n_cond_taken_predicted = 0;
+    this->n_cond_not_taken_predicted = 0;
+
+    this->last_entry = nullptr;
 }
 
-btb_entry_t *btb_t::get_entry(uint64_t address) {
+btb_entry_t *btb_t::get_entry(uint64_t address, uint64_t cicle) {
     // And here's why BUFFER_SIZE shall be a power of 2.
     // This gets exactly the number of bits we want from the address.
     //
@@ -37,6 +46,7 @@ btb_entry_t *btb_t::get_entry(uint64_t address) {
     // the oldest accessed.
     for (int i = 0; i < btb::BLOCK_SIZE; ++i) {
         if (this->buffer[b][i].address == address) {
+            this->buffer[b][i].last_access = cicle;
             return &this->buffer[b][i];
         }
     }
@@ -44,17 +54,17 @@ btb_entry_t *btb_t::get_entry(uint64_t address) {
     int idx = 0;
     for (int i = 0; i < btb::BLOCK_SIZE; ++i) {
         if (!this->buffer[b][i].valid) {
+            this->buffer[b][i].last_access = cicle;
             return &this->buffer[b][i];
         }
-        if (this->buffer[b][i].last_access <= this->buffer[b][idx].last_access) {
+        if (this->buffer[b][i].last_access <=
+            this->buffer[b][idx].last_access) {
             idx = i;
         }
     }
-    return &this->buffer[b][idx];
-}
 
-bool btb_t::fetch_uncond(uint64_t address, btb_entry_t *entry) {
-    return (entry->valid && entry->address == address);
+    this->buffer[b][idx].last_access = cicle;
+    return &this->buffer[b][idx];
 }
 
 void btb_t::set_uncond(uint64_t address, btb_entry_t *entry) {
@@ -65,9 +75,6 @@ void btb_t::set_uncond(uint64_t address, btb_entry_t *entry) {
 
 void btb_t::increase_stat(branch_t type) {
     switch (type) {
-        case BRANCH_COND:
-            this->n_cond++;
-            break;
         case BRANCH_UNCOND:
             this->n_uncond++;
             break;
@@ -77,7 +84,7 @@ void btb_t::increase_stat(branch_t type) {
         case BRANCH_CALL:
             this->n_calls++;
             break;
-        // Ignore RETURN as it's unreachable.
+        // Ignore RETURN and COND.
         default:
             break;
     }
@@ -100,27 +107,87 @@ void btb_t::increase_uncond_missed(branch_t type) {
     }
 }
 
-int btb_t::check_instruction(opcode_package_t *instruction, uint64_t cicle) {
-    if (instruction->opcode_operation == INSTRUCTION_OPERATION_BRANCH &&
-        instruction->branch_type != BRANCH_RETURN) {
-        btb_entry_t *entry = get_entry(instruction->opcode_address);
-        entry->last_access = cicle;
-        increase_stat(instruction->branch_type);
+int btb_t::check_uncond(opcode_package_t *instruction, btb_entry_t *entry) {
+    increase_stat(instruction->branch_type);
 
-        if (instruction->branch_type == BRANCH_COND) {
-            // TODO
+    bool hit = entry->valid && entry->address == instruction->opcode_address;
+    this->set_uncond(instruction->opcode_address, entry);
+
+    if (hit) return 0;
+
+    this->increase_uncond_missed(instruction->branch_type);
+    return btb::LATENCY_UNCOND;
+}
+
+void btb_t::prepare_cond(opcode_package_t *instruction, btb_entry_t *entry) {
+    this->last_address = instruction->opcode_address;
+    this->last_size = instruction->opcode_size;
+    this->last_entry = entry;
+}
+
+void btb_t::increase_cond_stats(bool is_taken, bool hit, bool predicted_taken) {
+    if (is_taken) {
+        this->n_cond_taken++;
+        if (!hit) {
+            this->n_cond_taken_missed++;
+        } else if (predicted_taken) {
+            this->n_cond_taken_predicted++;
+        }
+    } else {
+        this->n_cond_not_taken++;
+        if (!hit) {
+            this->n_cond_not_taken_missed++;
+        } else if (!predicted_taken) {
+            this->n_cond_not_taken_predicted++;
+        }
+    }
+}
+
+int btb_t::check_last(opcode_package_t *instruction) {
+    int latency = 0;
+    if (this->last_entry != nullptr) {
+        bool is_taken =
+            instruction->opcode_address == this->last_address + this->last_size;
+
+        if (this->last_entry->valid && this->last_entry->is_conditional) {
+            // Hit.
+            this->increase_cond_stats(is_taken, true,
+                                      this->last_entry->is_taken);
+            if (is_taken != this->last_entry->is_taken) {
+                this->last_entry->is_taken = !this->last_entry->is_taken;
+                latency = btb::LATENCY_COND;
+            }
         } else {
-            bool hit = btb_t::fetch_uncond(instruction->opcode_address, entry);
-            btb_t::set_uncond(instruction->opcode_address, entry);
-
-            if (hit) return 0;
-
-            this->increase_uncond_missed(instruction->branch_type);
-            return btb::LATENCY_UNCOND;
+            // Missed.
+            // predicted_taken is actually ignored in this call as hit is false.
+            this->increase_cond_stats(is_taken, false, false);
+            this->last_entry->address = this->last_address;
+            this->last_entry->is_conditional = true;
+            this->last_entry->is_taken = is_taken;
+            this->last_entry->valid = true;
+            latency = is_taken ? btb::LATENCY_COND : 0;
         }
     }
 
-    return 0;
+    return latency;
+}
+
+int btb_t::check_instruction(opcode_package_t *instruction, uint64_t cicle) {
+    int last_latency = this->check_last(instruction);
+    this->last_entry = nullptr;
+
+    if (instruction->opcode_operation == INSTRUCTION_OPERATION_BRANCH &&
+        instruction->branch_type != BRANCH_RETURN) {
+        btb_entry_t *entry = get_entry(instruction->opcode_address, cicle);
+
+        if (instruction->branch_type == BRANCH_COND) {
+            this->prepare_cond(instruction, entry);
+        } else {
+            return this->check_uncond(instruction, entry) + last_latency;
+        }
+    }
+
+    return last_latency;
 }
 
 void btb_t::statistics() {
@@ -133,4 +200,12 @@ void btb_t::statistics() {
                 this->n_missed_syscalls);
     ORCS_PRINTF("number of calls: %lu (missed: %lu)\n", this->n_calls,
                 this->n_missed_calls);
+    ORCS_PRINTF("number of taken conditionals: %lu (missed: %lu)\n",
+                this->n_cond_taken, this->n_cond_taken_missed);
+    ORCS_PRINTF("number of not taken conditionals: %lu (missed: %lu)\n",
+                this->n_cond_not_taken, this->n_cond_not_taken_missed);
+    ORCS_PRINTF("number of correctly predicted taken conditionals: %lu\n",
+                this->n_cond_taken_predicted);
+    ORCS_PRINTF("number of correctly predicted not taken conditionals: %lu\n",
+                this->n_cond_not_taken_predicted);
 }
--- btb.hpp
+++ btb.hpp
@@ -11,6 +11,7 @@ namespace btb {
 
 const int BLOCK_SIZE = 12;
 const int LATENCY_UNCOND = 12;
+const int LATENCY_COND = 512;
 
 const int BUFFER_BITS = 10;
 const int BUFFER_SIZE = 1 << BUFFER_BITS;
@@ -30,19 +31,33 @@ typedef btb_entry_t btb_block_t[btb::BLOCK_SIZE];
 class btb_t {
    private:
     btb_block_t buffer[btb::BUFFER_SIZE];
+
     uint64_t n_uncond;
     uint64_t n_syscalls;
     uint64_t n_calls;
     uint64_t n_missed_uncond;
     uint64_t n_missed_syscalls;
     uint64_t n_missed_calls;
-    uint64_t n_cond;
+
+    uint64_t n_cond_taken;
+    uint64_t n_cond_not_taken;
+    uint64_t n_cond_taken_missed;
+    uint64_t n_cond_not_taken_missed;
+    uint64_t n_cond_taken_predicted;
+    uint64_t n_cond_not_taken_predicted;
+
+    uint64_t last_address;
+    uint64_t last_size;
+    btb_entry_t *last_entry;
 
     void increase_stat(branch_t type);
     void increase_uncond_missed(branch_t type);
     void set_uncond(uint64_t address, btb_entry_t *entry);
-    bool fetch_uncond(uint64_t address, btb_entry_t *entry);
-    btb_entry_t *get_entry(uint64_t address);
+    int check_uncond(opcode_package_t *instruction, btb_entry_t *entry);
+    void prepare_cond(opcode_package_t *instruction, btb_entry_t *entry);
+    void increase_cond_stats(bool is_taken, bool hit, bool predicted_taken);
+    int check_last(opcode_package_t *instruction);
+    btb_entry_t *get_entry(uint64_t address, uint64_t cicle);
 
    public:
     btb_t();
@@ -50,7 +65,7 @@ class btb_t {
     // Returns the latency.
     int check_instruction(opcode_package_t *instruction, uint64_t cicle);
     inline uint64_t get_total_accesses() {
-        return this->n_calls + this->n_syscalls + this->n_cond + this->n_uncond;
+        return this->n_calls + this->n_syscalls + this->n_uncond;
     }
 };
 
